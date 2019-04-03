3e9d8cd062d1aa65f510b5ecc74049d489036274
李东齐 <Lidq@cailian.net>
2019-04-03 10:55:22 +0800

    算法

--- src/main/java/com/company/project/compute/printListFromTailToHead.java
+++ src/main/java/com/company/project/compute/printListFromTailToHead.java
@@ -1,4 +1,41 @@
 package com.company.project.compute;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ *输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+ */
 public class printListFromTailToHead {
+
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        listNode.next = listNode2;
+        listNode2.next = new ListNode(3);
+        System.out.println(printListFromTailToHead(listNode));
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
 }
