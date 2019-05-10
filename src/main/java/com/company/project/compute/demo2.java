5163f0afcb8497246be53cf16a25d52c410dcaaa
李东齐 <Lidq@cailian.net>
2019-05-10 13:54:19 +0800

    算法

--- src/main/java/com/company/project/compute/demo2.java
+++ src/main/java/com/company/project/compute/demo2.java
@@ -9,22 +9,16 @@ import java.util.LinkedList;
  *
  */
 public class demo2 {
-    class Node {
-        int data;
-        Node left;
-        Node right;
-    }
 
-    public class CompleteTreeChecker {
         //实现广度遍历需要的队列
-        private LinkedList<Node> queue = new LinkedList<Node>();
+        private LinkedList<Node> list = new LinkedList<Node>();
         //第n层最右节点的标志
         private boolean leftMost = false;
 
         public boolean processChild(Node child) {
             if(child != null) {
                 if(!leftMost) {
-                    queue.addLast(child);
+                    list.addLast(child);
                 } else {
                     return false;
                 }
@@ -39,10 +33,10 @@ public class demo2 {
             if(root == null) return true;
 
             //首先根节点入队列
-            queue.addLast(root);
+            list.addLast(root);
 
-            while(!queue.isEmpty()) {
-                Node node = queue.removeFirst();
+            while(!list.isEmpty()) {
+                Node node = list.removeFirst();
 
                 //处理左子结点
                 if(!processChild(node.left))
@@ -55,5 +49,25 @@ public class demo2 {
             //广度优先遍历完毕，此树是完全二叉树
             return true;
         }
+
+    public static void main(String[] args) {
+       Node node1 = new Node(1);
+       Node node2 = new Node(2);
+       Node node3 = new Node(3);
+       Node node4 = new Node(4);
+       node1.left = node2;
+       node1.right = node3;
+        node2.left = node4;
+        demo2 demo2 = new demo2();
+        System.out.println( demo2.isCompleteTree(node1));
+
+    }
+}
+class Node {
+    int data;
+    Node left = null;
+    Node right =null;
+    Node(int data){
+        this.data = data;
     }
 }
