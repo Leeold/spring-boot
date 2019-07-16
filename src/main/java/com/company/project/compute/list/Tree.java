61c06309e079076cd5d0ca30862f385f2a433eb0
lidongqi <254750646@qq.com>
2019-07-16 10:43:25 +0800

    算法

--- src/main/java/com/company/project/compute/list/Tree.java
+++ src/main/java/com/company/project/compute/list/Tree.java
@@ -1,5 +1,6 @@
 package com.company.project.compute.list;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Tree {
@@ -81,5 +82,50 @@ public class Tree {
         }
 
         return max;
+    }
+
+    /**
+     * 判断一棵二叉树是不是完全二叉树
+     */
+
+    public static boolean isTree(TreeNode treeNode){
+        if(treeNode == null){
+            return false;
+        }
+        boolean isT = false;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(treeNode);
+        while (queue.size() > 0){
+            TreeNode node = queue.poll();
+            if(isT && (node.left !=null || node.right != null)){
+                return false;
+            }
+            if(node.left ==null && node.right!=null){
+                return false;
+            }
+            if(node.left !=null){
+                queue.offer(node.left);
+            }
+            if(node.right != null){
+                queue.offer(node.right);
+            }else{
+                isT = true;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        node1.left = node2;
+        node1.right = node3;
+//        node2.left = node4;
+        node2.right = node5;
+        boolean is = isTree(node1);
+        System.out.println(is);
     }
 }
