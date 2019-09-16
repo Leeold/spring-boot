026ed8722cbc8d4bbaee046a3df49fd78e76070d
lidongqi <254750646@qq.com>
2019-09-16 09:56:24 +0800

    测试

---
+++ src/main/java/com/company/project/designPattern/Demo.java
@@ -0,0 +1,102 @@
+package com.company.project.designPattern;
+
+
+import com.company.project.model.User;
+import com.sun.media.jfxmediaimpl.HostUtils;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+class ShareResource{
+    private int number =1;
+    private Lock lock = new ReentrantLock();
+    private Condition c1 = lock.newCondition();
+    private Condition c2 = lock.newCondition();
+    private Condition c3 = lock.newCondition();
+    public void print5(){
+        lock.lock();
+        try {
+            while(number != 1){
+                c1.await();
+            }
+
+            for (int i=1;i<= 5;i++){
+                System.out.println(Thread.currentThread().getName()+"\t"+i);
+            }
+
+            number =2;
+            c2.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+    public void print10(){
+        lock.lock();
+        try {
+            while(number != 2){
+                c2.await();
+            }
+
+            for (int i=1;i<= 10;i++){
+                System.out.println(Thread.currentThread().getName()+"\t"+i);
+            }
+
+            number =3;
+            c3.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+    public void print15(){
+        lock.lock();
+        try {
+            while(number != 3){
+                c3.await();
+            }
+
+            for (int i=1;i<= 15;i++){
+                System.out.println(Thread.currentThread().getName()+"\t"+i);
+            }
+
+            number =1;
+            c1.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+}
+
+
+public class Demo {
+    private static Demo instance = null;
+    private Demo(){
+        System.out.println(Thread.currentThread().getName()+"我是构造方法");
+    }
+    public static Demo getInstance(){
+        if(instance == null){
+            instance = new Demo();
+        }
+        return instance;
+    }
+    public static void main(String[] args) {
+        System.out.println(Demo.getInstance() == Demo.getInstance());
+        System.out.println(Demo.getInstance() == Demo.getInstance());
+    }
+
+
+}
