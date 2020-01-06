be3477a1335bb851d2f3d855597832b40820514b
lidongqi <254750646@qq.com>
2020-01-06 09:59:59 +0800

    service

--- src/main/java/com/company/project/demo/TestLambda.java
+++ src/main/java/com/company/project/demo/TestLambda.java
@@ -1,53 +1,57 @@
 package com.company.project.demo;
 
+import com.google.common.base.Strings;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.val;
 import sun.misc.BASE64Encoder;
 
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.*;
 import java.util.stream.Collectors;
-
+@Getter
+@ToString
+@AllArgsConstructor
+class User {
+    String userName;
+    int age;
+}
 public class TestLambda {
-    public static String reverseWords(String s) {
-        String[] strs = s.split(" ");
-        System.out.println(strs);
-        StringBuilder res = new StringBuilder();
-        for(int i = strs.length - 1; i >= 0; i--)
-        {
-            if(strs[i].length() > 0)
-            {
-                res.append(strs[i]);
-                res.append(" ");
-            }
-        }
-        return res.toString();
-    }
+
 
     public static void main(String[] args) {
-        String str = "www.toutiao.com";
-        String[] arr = str.split(" . ");
-        System.out.println(Arrays.toString(arr));
+        BlockingQueue<String> blockingQueue = new SynchronousQueue<>();
+        new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t put 1");
+                blockingQueue.put("1");
+                System.out.println(Thread.currentThread().getName() + "\t put 2");
+                blockingQueue.put("2");
+                System.out.println(Thread.currentThread().getName() + "\t put 3");
+                blockingQueue.put("3");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "AAA").start();
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println(Thread.currentThread().getName() + "\ttake " + blockingQueue.take());
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println(Thread.currentThread().getName() + "\ttake " + blockingQueue.take());
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println(Thread.currentThread().getName() + "\ttake " + blockingQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "BBB").start();
     }
-
-}
-class singleton{
-
-
-    private static singleton instance;
-
-    private singleton(){}
-
-    public static singleton getInstance(){
-       if (instance == null) {
-           instance = new singleton();
-       }
-       return instance;
-    }
-
-
-
-
-
 }
