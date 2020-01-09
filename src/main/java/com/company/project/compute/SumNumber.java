be2c4cb76778a8410f55ffc36733fcbccb8d4bb2
lidongqi <254750646@qq.com>
2020-01-09 16:33:10 +0800

    反转链表

---
+++ src/main/java/com/company/project/compute/SumNumber.java
@@ -0,0 +1,20 @@
+package com.company.project.compute;
+
+
+/**
+ * 求递增数组中相加等于10的元素对(使用冒泡排序)
+ */
+public class SumNumber {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6,7,8,9};
+        int num = 0;
+        for(int i=arr.length-1;i>0;i--){
+            for(int j =0; j<i;j++){
+                if(arr[i]+arr[j] == 10){
+                    num++;
+                }
+            }
+        }
+        System.out.println(num);
+    }
+}
